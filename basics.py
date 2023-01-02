9cfa91ec56ee15193ab10cf1e3bd31367810272f
Pruthvi383 <121416113+Pruthvi383@users.noreply.github.com>
2023-01-02 12:32:46 +0530

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,55 @@
+Python 3.10.7 (v3.10.7:6cc6b13308, Sep  5 2022, 14:02:52) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> int(2.0)
+2
+>>> float(2)
+2.0
+>>> a=2+i
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    a=2+i
+NameError: name 'i' is not defined. Did you mean: 'id'?
+>>> matn
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    matn
+NameError: name 'matn' is not defined
+>>> math.pi
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    math.pi
+NameError: name 'math' is not defined
+>>> import math
+>>> math.pi
+3.141592653589793
+>>> math.sqrt(36)
+6.0
+>>> write a program to get the user input number and find the the sqrt.
+SyntaxError: invalid syntax
+>>> int(25.0)
+25
+>>> math.sqrt(25)
+5.0
+>>> int(81.0)
+81
+>>> math.sqrt(81)
+9.0
+>>> a=int(input("enter the number"))
+enter the number 45
+>>> math.sqrt(a)
+6.708203932499369
+>>> 
+========================================================================= RESTART: /Users/pruthvipatil/Documents/addition.py =========================================================================
+enter the first number8
+enter the second number9
+17
+>>> c=a*b
+>>> d=a*b
+>>> d
+72
+>>> t=a-b
+>>> t
+-1
+>>> y=a/b
+>>> y
+0.8888888888888888
